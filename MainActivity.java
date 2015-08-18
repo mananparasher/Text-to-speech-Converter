c88c151b7e46519671fd0fc3d31535e611e9141a
mananparasher <mananparasher@gmail.com>
2015-08-18 21:58:08 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.ttsc;
+
+import java.util.Locale;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity implements OnClickListener 
+{
+
+	EditText e1;
+	ImageButton i1;
+	TextToSpeech tts;
+	String msg;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+        
+        
+        
+        e1=(EditText)findViewById(R.id.e1);
+        i1=(ImageButton)findViewById(R.id.i1);
+        i1.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+	@Override
+	public void onClick(View arg0) 
+	{
+		
+	 msg=	e1.getText().toString();
+		
+		tts=new TextToSpeech(this, new TextToSpeech.OnInitListener()
+		{
+			
+			@Override
+			public void onInit(int arg0)
+			{
+				tts.setLanguage(Locale.US);
+			tts.speak(msg, TextToSpeech.QUEUE_FLUSH, null);
+				
+			}
+		});
+		
+	}
+
+}
